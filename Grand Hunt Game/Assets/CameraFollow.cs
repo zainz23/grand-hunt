0e0429d01514caafb22dc85b5c0e76f598917777
gomez-eric <gomez123098@yahoo.com>
2018-12-07 18:52:06 -0600

    CAMERA
    CAMERA FOLLOW CHARACTER

---
+++ Grand Hunt Game/Assets/CameraFollow.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public GameObject target;
+    public float damping = 1;
+    Vector3 offset;
+
+    void Start()
+    {
+        offset = transform.position - target.transform.position;
+    }
+
+    void LateUpdate()
+    {
+        Vector3 desiredPosition = target.transform.position + offset;
+        Vector3 position = Vector3.Lerp(transform.position, desiredPosition, Time.deltaTime * damping);
+        transform.position = position;
+
+        transform.LookAt(target.transform.position);
+    }
+}
