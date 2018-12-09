26529ac7623d0932ea3e4b8f54908ac0d6e1d6df
zainz23 <zzahra2@uic.edu>
2018-12-08 23:55:34 -0600

    Added Prop Transforming!
    This new script attaches to player and allows them to transform into any object that we choose. Currently, I've only added the abilit to transform into a barrel.

---
+++ Grand Hunt Game/Assets/Scripts/PropHunt.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// To add props
+// Make a public GameObject designated for that prop
+// In Start(), set the GameObject to false
+public class PropHunt : MonoBehaviour
+{
+    public GameObject Player;   // The player that is being disappeared
+    public GameObject Barrel;   // What you turn into
+    /* Can add loads more here */
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Player.SetActive(true);
+        Barrel.SetActive(false);
+    }
+
+    // While player is colliding with object...
+    void OnTriggerStay(Collider coll)
+    {
+        // Tag the object in game
+        // Note: Children of Player are left untagged.
+        if (coll.gameObject.tag == "Barrel")
+        {
+            if (Input.GetKeyDown(KeyCode.E) )
+            {
+                // Player wants to become a barrel
+                Barrel.SetActive(true);
+                // Player no longer exists
+                Player.SetActive(false);
+                /* Set any other props to false aswell */
+
+            }
+        }
+    }
+}
