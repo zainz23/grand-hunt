1c5d6e7e7a1266f1cf18a5be0c668e0621de184b
gomez-eric <gomez123098@yahoo.com>
2018-12-09 19:05:41 -0600

    MESH SWITCH
    STARTING GM AND ALSO CHANGES PROP OBJ MESH

--- Grand Hunt Game/Assets/Scripts/PropHunt.cs
+++ Grand Hunt Game/Assets/Scripts/PropHunt.cs
@@ -13,19 +13,14 @@ using UnityEngine;
 public class PropHunt : MonoBehaviour
 {
     public GameObject Player;   // The player that is being disappeared
-    public GameObject Barrel;   // What you turn into
-
-    /* Can add loads more here */
-    public GameObject Yellow_Tree;
-    public GameObject Wheel_Barrow;
+    public GameObject PROP;   // What you turn into
     // Start is called before the first frame update
     void Start()
     {
         // On game start, only the player is visible.
         Player.SetActive(true);
-        Barrel.SetActive(false);
-        Yellow_Tree.SetActive(false);
-        Wheel_Barrow.SetActive(false);
+        PROP.SetActive(false);
+        
     }
 
     void Update()
@@ -33,55 +28,36 @@ public class PropHunt : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Z))
         {
             Player.SetActive(true);
-            Barrel.SetActive(false);
-            Yellow_Tree.SetActive(false);
-            Wheel_Barrow.SetActive(false);
+            PROP.SetActive(false);
         }
     }
 
     // While player is colliding with object...
     void OnTriggerStay(Collider coll)
     {
+        
         // Tag the object in game
         // Note: Children of Player are left untagged.
-        if (coll.gameObject.tag == "Barrel")
+        if (coll.gameObject.tag == "PROP")
         {
             if (Input.GetKeyDown(KeyCode.E) )
             {
+                Debug.Log(coll.GetComponent<MeshFilter>().mesh.name);
+                MeshFilter mf = this.GetComponentInChildren<MeshFilter>();
+                mf.mesh = coll.GetComponent<MeshFilter>().mesh;
+
+                mf.GetComponent<Transform>().rotation = coll.transform.rotation;
+
                 // Player wants to become a barrel
-                Barrel.SetActive(true);
+                PROP.SetActive(true);
+
                 // Player no longer exists
                 Player.SetActive(false);
-                /* Set any other props to false aswell */
-                Yellow_Tree.SetActive(false);
-                Wheel_Barrow.SetActive(false);
+               
 
             }
         }
-        if (coll.gameObject.tag == "Yellow_Tree")
-        {
-            if (Input.GetKeyDown(KeyCode.E) )
-            {
-                // Player wants to become a Yellow_Tree
-                Yellow_Tree.SetActive(true);
-                // Player + Props become inactive
-                Player.SetActive(false);
-                Barrel.SetActive(false);
-                Wheel_Barrow.SetActive(false);
-            }
-        }
-        if (coll.gameObject.tag == "Wheel_Barrow")
-        {
-            if (Input.GetKeyDown(KeyCode.E))
-            {
-                // Player wants to become a Wheel_Barrow
-                Wheel_Barrow.SetActive(true);
-                // Player + Props become inactive
-                Player.SetActive(false);
-                Barrel.SetActive(false);
-                Yellow_Tree.SetActive(false);
-            }
-        }
+        
 
     }
 }
