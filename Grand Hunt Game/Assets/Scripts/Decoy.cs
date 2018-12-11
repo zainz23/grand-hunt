3419a1dbddd63bf1207d865cbc2e6ede9f3ba3e7
zainz23 <zzahra2@uic.edu>
2018-12-10 23:19:38 -0600

    Fixed Decoy.cs
    Decoy now works with our original character.
    *New scale calculation

--- Grand Hunt Game/Assets/Scripts/Decoy.cs
+++ Grand Hunt Game/Assets/Scripts/Decoy.cs
@@ -5,20 +5,24 @@ using UnityEngine;
 public class Decoy : MonoBehaviour
 {
     public GameObject PROP;
+    public MeshFilter MESHFILTER;
     float X, Y, Z;
 
-    // Update is called once per frame
     void Update()
     {
-        X = PROP.transform.localScale.x;
-        Y = PROP.transform.localScale.y;
-        Z = PROP.transform.localScale.y;
+
 
         // Press C to make a copy in-game
         if (Input.GetKeyDown(KeyCode.C))
         {
+            X = PROP.transform.localScale.x;
+            Y = PROP.transform.localScale.y;
+            Z = PROP.transform.localScale.y;
+
             GameObject decoy = Instantiate(PROP, transform.position, transform.rotation);
-            decoy.transform.localScale = new Vector3(X - 3, Y - 3, Z- 3);
+            // Since our character is small, calculate new scale.
+            decoy.transform.localScale = new Vector3(X * 0.25f, Y * 0.25f, Z * 0.25f);
+
             decoy.transform.localPosition = this.gameObject.transform.localPosition;
 
         }
