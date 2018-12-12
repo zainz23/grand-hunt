07b35d96c49caa2a95346883fef0bc77bc342d06
gomez-eric <gomez123098@yahoo.com>
2018-12-12 03:18:40 -0600

    DECOY CODE
    ADDED A SMOKE EFFECT WHEN SEEKER TOUCHES DECOY & ALLOWING PLAYER TO HIDE BEFORE BEING FOUND;
    (CURRENTLY USING PLAYER FOR TESTING);
    PROPS WILL ONLY RENDER IF OBJECT IS NOT IN WAY (BEHIND PLAYER);

---
+++ Grand Hunt Game/Assets/DecoySmoke.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DecoySmoke : MonoBehaviour
+{
+
+    public GameObject smoke;
+    public GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.transform.tag == "Player") {
+            GameObject decoy = Instantiate(smoke, transform.position, transform.rotation);
+            Destroy(decoy, decoy.GetComponent<ParticleSystem>().main.duration + 5f);
+            Destroy(this.gameObject, 0.25f);
+        }
+
+
+    }
+}
--- Grand Hunt Game/Assets/Scripts/Decoy.cs
+++ Grand Hunt Game/Assets/Scripts/Decoy.cs
@@ -6,6 +6,7 @@ public class Decoy : MonoBehaviour
 {
     public GameObject PROP;
     public MeshFilter MESHFILTER;
+    public GameObject smokeprefab;
 
     void Start()
     {
@@ -18,25 +19,54 @@ public class Decoy : MonoBehaviour
         // Press C to make a copy in-game
         if (Input.GetKeyDown(KeyCode.C) && PROP.activeSelf)
         {
-            
+
             // Sets its local orientation rather than its global orientation.
-            PROP.transform.SetParent(PROP.transform.parent, false);
-            GameObject decoy = Instantiate(PROP, PROP.transform.position, PROP.transform.rotation);
-            decoy.AddComponent<BoxCollider>();
-            decoy.tag = "PROP";
+            //PROP.transform.SetParent(PROP.transform.parent, false);
 
-            // Since our character is small, calculate new scale.
-            float X, Y, Z;
-            // TAKES PROPS LOCAL TRANSFORM AND MULTIPLIES WITH PLAYERS LOCAL TRANSFORM
-            X = PROP.transform.localScale.x * transform.localScale.x;
-            Y = PROP.transform.localScale.y * transform.localScale.y;
-            Z = PROP.transform.localScale.z * transform.localScale.z;
-            decoy.transform.localScale = new Vector3(X, Y, Z);
+            //SPAWNS OBJECT BEHIND PLAYER
 
-            // THIS IS ORIGINAL SIZE OF THE OBJECTS
-            //decoy.transform.localScale = new Vector3(1, 1, 1);
+            // RAYCAST TO INSPECT IF OBJECT EXIST BEFORE CREATING OBJECT
+            RaycastHit hit;
 
-            decoy.transform.localPosition = this.gameObject.transform.localPosition;
+            float distance = (0.35f);
+            Vector3 spawnPosition = transform.position;
+            spawnPosition.y += 0.02f;
+
+            // IF THERE IS ALREADY AN OBJECT THAT EXIST DONT SPAWN OBJECT
+            if (Physics.Raycast(spawnPosition, transform.TransformDirection(Vector3.forward) * -distance, out hit, distance))
+            {
+                Debug.DrawRay(spawnPosition, transform.TransformDirection(Vector3.forward) * -distance, Color.red);
+                Debug.Log("HIT SO NO SPAWN" + hit.collider.name);
+            
+            // IF OBJECT IS NOT IN WAY SPAWN DECOY OBJECT
+            } else {
+                Debug.DrawRay(spawnPosition, transform.TransformDirection(Vector3.forward) * -5f, Color.cyan);
+                Debug.Log("NO HIT, OBJECT SPAWN");
+
+                GameObject decoy = Instantiate(PROP, transform.position + transform.forward * -(0.25f), PROP.transform.rotation);
+                //decoy.transform.localPosition = this.gameObject.transform.localPosition;
+                decoy.AddComponent<Rigidbody>();
+                decoy.AddComponent<BoxCollider>();
+
+                decoy.AddComponent<DecoySmoke>();
+                decoy.GetComponent<DecoySmoke>().smoke = smokeprefab;
+
+                decoy.tag = "PROP";
+
+
+                // Since our character is small, calculate new scale.
+                float X, Y, Z;
+                // TAKES PROPS LOCAL TRANSFORM AND MULTIPLIES WITH PLAYERS LOCAL TRANSFORM
+                X = PROP.transform.localScale.x * transform.localScale.x;
+                Y = PROP.transform.localScale.y * transform.localScale.y;
+                Z = PROP.transform.localScale.z * transform.localScale.z;
+                decoy.transform.localScale = new Vector3(X, Y, Z);
+                //decoy.transform.localScale = new Vector3(1, 1, 1);
+
+
+            }
+
+
 
         }
 
