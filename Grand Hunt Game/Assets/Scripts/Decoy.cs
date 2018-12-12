c7ff9cc3975c1ed1b51d5a9fe32f6040ad5c6df2
zainz23 <zzahra2@uic.edu>
2018-12-11 19:54:03 -0600

    Working Decoy.cs
    Need to change design of scaling, but this will suffice (for now)

--- Grand Hunt Game/Assets/Scripts/Decoy.cs
+++ Grand Hunt Game/Assets/Scripts/Decoy.cs
@@ -11,7 +11,6 @@ public class Decoy : MonoBehaviour
     void Update()
     {
 
-
         // Press C to make a copy in-game
         if (Input.GetKeyDown(KeyCode.C))
         {
@@ -19,10 +18,15 @@ public class Decoy : MonoBehaviour
             Y = PROP.transform.localScale.y;
             Z = PROP.transform.localScale.y;
 
-            GameObject decoy = Instantiate(PROP, transform.position, transform.rotation);
-            // Since our character is small, calculate new scale.
-            decoy.transform.localScale = new Vector3(X * 0.25f, Y * 0.25f, Z * 0.25f);
+            // rotation.eulerAngles = new Vector3(0, 90, 0);
+            // Sets its local orientation rather than its global orientation.
+            PROP.transform.SetParent(PROP.transform.parent, false);
+            GameObject decoy = Instantiate(PROP, PROP.transform.position, PROP.transform.rotation);
 
+            // Since our character is small, calculate new scale.
+
+            decoy.transform.localScale = new Vector3(X * 0.25f, Y * 0.25f, Z * 0.25f);
+            // decoy.transform.localScale = new Vector3(X, Y, Z );
             decoy.transform.localPosition = this.gameObject.transform.localPosition;
 
         }
