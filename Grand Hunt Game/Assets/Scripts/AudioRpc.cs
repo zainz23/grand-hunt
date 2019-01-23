79706bac3d4b45f33313b4c9a4f79767b5a5c831
zz <32082888+zainz23@users.noreply.github.com>
2019-01-23 13:55:56 -0600

    Script for Audio RPC

---
+++ Grand Hunt Game/Assets/Scripts/AudioRpc.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class AudioRpc : Photon.MonoBehaviour
+{
+
+    // The logic behind the audio is unsure yet
+    // As in, should we auto play audio every 30s
+    // Or allow users to ping. Maybe even both
+    // Later we can have customizable pings
+    // That players can unlock
+    [Tooltip("Sound for hiders/proppers")]
+    public AudioClip ping;
+
+    AudioSource p_Source;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        p_Source = GetComponent<AudioSource>();
+    }
+
+    [PunRPC]
+    void Ping()
+    {
+        if (!this.enabled)
+        {
+            return;
+        }
+        p_Source.clip = ping;
+        p_Source.Play();
+    }
+
+    private void OnApplicationFocus(bool focus)
+    {
+        this.enabled = focus;
+    }
+}
